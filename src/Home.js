6df3e9174c77a9b51a289a5a149054af76fe4a62
guillermo.333 <guillermo.3@hotmail.com.ar>
2022-12-11 00:01:53 +0100

    algo

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Home extends Component{
+    render(){ 
+        return(
+            <div>
+            <p>Nombre: El nombre</p>
+            <p>Precio: El precio</p>
+            </div>
+        )
+    }
+}
+
+export default Home
